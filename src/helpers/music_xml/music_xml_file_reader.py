ebe5e2abf12a0c8c2a3ddb47b6753bc5bad4fe7b
ko10ok <home_r@mail.ru>
2020-06-21 22:58:18 +0700

    zip compressed musix xml reading

--- src/helpers/music_xml/music_xml_file_reader.py
+++ src/helpers/music_xml/music_xml_file_reader.py
@@ -1,3 +1,6 @@
+import os
+import zipfile
+
 from xmltodict import parse as xml_parse
 
 
@@ -5,3 +8,13 @@ def read_music_xml(file_path):
     with open(file_path) as music_xml_file:
         result = music_xml_file.read()
         return xml_parse(result, force_list=['score-part', 'part', 'note', 'measure', 'creator'])
+
+
+def read_compressed_music_xml(file_path):
+    file = zipfile.ZipFile(file_path, 'r')
+
+    name = os.path.basename(file_path)
+    music_xml_file = file.open(name.replace('.mxl', '.xml'), 'r')
+
+    result = music_xml_file.read()
+    return xml_parse(result, force_list=['score-part', 'part', 'note', 'measure', 'creator'])
